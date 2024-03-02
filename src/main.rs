bdfd487a9099ac471acf95fabdac97df0eb93a53
Wexler763 <danylkarp@gmail.com>
2024-03-02 22:22:25 +0200

    76_0.3

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ use letsgetrusty::startup::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
-    // Renamed!
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
+    println!("Connected");
     run(listener, connection_pool)?.await
 }
