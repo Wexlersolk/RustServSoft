96c4aa791ead03c64595e7bdf46c809fe0d1fe2b
karina2003 <144662586+kar1na2003@users.noreply.github.com>
2024-03-17 15:56:12 +0200

    v76_0.9

--- migrations/20240229124329_init.up.sql
+++ migrations/20240229124329_init.up.sql
@@ -14,6 +14,26 @@ CREATE TABLE IF NOT EXISTS user_table (
     "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
 
+CREATE TABLE IF NOT EXISTS book_table (
+    "book_id" uuid PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
+    "name" varchar NOT NULL,
+    "author" varchar NOT NULL,
+    "scores" int NOT NULL,
+    "cost" int NOT NULL, 
+    "file_name" varchar NOT NULL,
+    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS comments_table (
+    "comment_id" uuid PRIMARY KEY NOT NULL DEFAULT (uuid_generate_v4()),
+    "comment_text" TEXT NOT NULL,
+    "comment_author" VARCHAR NOT NULL,
+    "commented_book" varchar NOT NULL,
+    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+
+);
 
 INSERT INTO access_table ("access_id", "group_name") VALUES 
 (1, 'regular'),
--- src/startup.rs
+++ src/startup.rs
@@ -1,4 +1,4 @@
-use crate::routes::{health_check, new_user, get_all_users};
+use crate::routes::{health_check, new_user, get_all_users, new_book, get_all_books, new_comment, get_all_comments};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
@@ -11,6 +11,10 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
             .route("/health_check", web::get().to(health_check))
             .route("/get_all_users", web::get().to(get_all_users))
             .route("/new_user", web::post().to(new_user))
+            .route("/new_book", web::post().to(new_book))
+            .route("/get_all_books", web::get().to(get_all_books))
+            .route("/new_comment", web::post().to(new_comment))
+            .route("get_all_comments", web::get().to(get_all_comments))
             .app_data(db_pool.clone())
     })
     .listen(listener)?
