6b0a9bda17899a36b96ac6d520c570c9474b5ce5
Mykola <nikomyk79@gmail.com>
2024-05-13 18:44:43 +0300

    made sorted books work

--- src/routes/bookdisplay.rs
+++ src/routes/bookdisplay.rs
@@ -1,27 +1,22 @@
 use crate::routes::*;
 
 use actix_web::{
-    web::{self, Bytes},
+    web::{self},
     HttpResponse,
 };
-use chrono::Utc;
 use sqlx::PgPool;
 
 #[derive(serde::Deserialize, serde::Serialize)]
 pub struct SortedInfo {
     genre: String,
-    parameter: String
-}
-
-pub async fn get_books_from_db(pool: web::Data<PgPool>, query: String) -> Result<Vec<BookData>, sqlx::Error> {
-    sqlx::query_as!(BookData, query)       
-        .fetch_all(pool.as_ref())
-        .await
+    parameter: String,
 }
 
 pub async fn get_all_books(pool: web::Data<PgPool>) -> HttpResponse {
-    let query = "SELECT book_view.*, '' as img FROM book_view";
-    match get_books_from_db(pool, query.to_string()).await {
+    match sqlx::query_as!(BookData, "SELECT book_view.*, '' as img FROM book_view",)
+        .fetch_all(pool.as_ref())
+        .await
+    {
         Ok(books) => {
             log::info!("All books have been fetched");
             HttpResponse::Ok().json(create_reduced_info_json(books))
@@ -35,16 +30,17 @@ pub async fn get_all_books(pool: web::Data<PgPool>) -> HttpResponse {
 
 pub async fn get_sorted_books(
     pool: web::Data<PgPool>,
-    sorting_params: web::Query<SortedInfo>
+    sorting_params: web::Query<SortedInfo>,
 ) -> HttpResponse {
-
-    let query = format!(
-        "SELECT book_view.*, '' as img FROM book_view WHERE genre_name = {} ORDER BY {} DESC",
+    match sqlx::query_as!(
+        BookData,
+        "SELECT book_view.*, '' as img FROM book_view WHERE genre_name = $1 ORDER BY $2 DESC",
         sorting_params.genre,
         sorting_params.parameter
-    );
-
-    match get_books_from_db(pool, query).await {
+    )
+    .fetch_all(pool.as_ref())
+    .await
+    {
         Ok(books) => {
             log::info!("All books have been fetched");
             HttpResponse::Ok().json(books)
