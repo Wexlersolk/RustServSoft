0133af1f6bb34b43196240f8cbd7277695d124c7
Wexler763 <danylkarp@gmail.com>
2024-05-13 18:28:26 +0300

    sorted books

--- src/routes/bookdisplay.rs
+++ src/routes/bookdisplay.rs
@@ -1,6 +1,5 @@
 use crate::routes::*;
 
-
 use actix_web::{
     web::{self, Bytes},
     HttpResponse,
@@ -8,14 +7,21 @@ use actix_web::{
 use chrono::Utc;
 use sqlx::PgPool;
 
-pub async fn get_books_from_db(pool: web::Data<PgPool>) -> Result<Vec<BookData>, sqlx::Error> {
-    sqlx::query_as!(BookData, "SELECT book_view.*, '' as img FROM book_view")
+#[derive(serde::Deserialize, serde::Serialize)]
+pub struct SortedInfo {
+    genre: String,
+    parameter: String
+}
+
+pub async fn get_books_from_db(pool: web::Data<PgPool>, query: String) -> Result<Vec<BookData>, sqlx::Error> {
+    sqlx::query_as!(BookData, query)       
         .fetch_all(pool.as_ref())
         .await
 }
 
 pub async fn get_all_books(pool: web::Data<PgPool>) -> HttpResponse {
-    match get_books_from_db(pool).await {
+    let query = "SELECT book_view.*, '' as img FROM book_view";
+    match get_books_from_db(pool, query.to_string()).await {
         Ok(books) => {
             log::info!("All books have been fetched");
             HttpResponse::Ok().json(create_reduced_info_json(books))
@@ -26,26 +32,19 @@ pub async fn get_all_books(pool: web::Data<PgPool>) -> HttpResponse {
         }
     }
 }
+
 pub async fn get_sorted_books(
     pool: web::Data<PgPool>,
-    sorting_params: web::Query<(String,)>,
+    sorting_params: web::Query<SortedInfo>
 ) -> HttpResponse {
-    let parameter = &sorting_params.0 .0; // Access the inner value of the tuple
-
-    let safe_parameter = match parameter.as_str() {
-        "cost" | "score" | "downloads" => parameter,
-        _ => {
-            log::error!("Invalid sorting parameter: {:?}", sorting_params);
-            return HttpResponse::BadRequest().finish();
-        }
-    };
 
     let query = format!(
-        "SELECT book_view.*, '' as img FROM book_view ORDER BY {} DESC",
-        safe_parameter
+        "SELECT book_view.*, '' as img FROM book_view WHERE genre_name = {} ORDER BY {} DESC",
+        sorting_params.genre,
+        sorting_params.parameter
     );
 
-    match get_books_from_db(pool).await {
+    match get_books_from_db(pool, query).await {
         Ok(books) => {
             log::info!("All books have been fetched");
             HttpResponse::Ok().json(books)
@@ -56,4 +55,3 @@ pub async fn get_sorted_books(
         }
     }
 }
-
