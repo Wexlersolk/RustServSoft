8dd82a10575650acbaf0eed8989750502010e406
Mykola <nikomyk79@gmail.com>
2024-05-14 11:47:04 +0300

    removed useless imports

--- src/routes/mainpage.rs
+++ src/routes/mainpage.rs
@@ -1,35 +1,26 @@
 use actix_web::{
-    web::{self, Bytes},
+    web::{self},
     HttpResponse,
 };
-use chrono::Utc;
-use serde_json::{json, Value};
 use sqlx::PgPool;
 
-
 #[derive(serde::Deserialize, serde::Serialize)]
-pub struct GenresData{
+pub struct GenresData {
     pub genre_name: Option<String>,
-    
 }
 
 pub async fn get_all_genres(pool: web::Data<PgPool>) -> HttpResponse {
-    let query = sqlx::query_as!(
-    GenresData,
-    "Select genre_name FROM genre_table ")
+    let query = sqlx::query_as!(GenresData, "Select genre_name FROM genre_table ")
         .fetch_all(pool.as_ref())
         .await;
     match query {
         Ok(genres) => {
             log::info!("Genres have been fetched");
-        HttpResponse::Ok().json(serde_json::to_value(&genres).unwrap())
-}
+            HttpResponse::Ok().json(serde_json::to_value(&genres).unwrap())
+        }
         Err(e) => {
             log::info!("Failed to fetch genres {}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
 }
-
-
-
